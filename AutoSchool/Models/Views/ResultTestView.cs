7df8686775b949a875c9920a9b05fc41ff9f8630
Ilya Gubanov <ilya.gubanov.4.7.8@gmail.com>
2022-06-03 00:03:13 +0300

    add ResultTestView

---
+++ AutoSchool/Models/Views/ResultTestView.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoSchool.Models.Views
+{
+    public class ResultTestView
+    {
+        public long Id { get; set; }
+        public string Status { get; set; }
+        public int AmountRightQuestions { get; set; }
+        public int AmountWrongQuestions { get; set; }
+        public double Result { get; set; }
+        public DateTime Date { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}

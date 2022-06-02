5f13c99de2d7a87ef14ce54f92eb593af692213d
Ilya Gubanov <ilya.gubanov.4.7.8@gmail.com>
2022-06-03 00:03:43 +0300

    add TestView

---
+++ AutoSchool/Models/Views/TestView.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoSchool.Models.Views
+{
+    public class TestView
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public List<QuestionView> Questions { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}

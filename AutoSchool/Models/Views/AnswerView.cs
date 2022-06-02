ad63c2b6bd22df6a6bbce8a5feef69ea12760b49
Ilya Gubanov <ilya.gubanov.4.7.8@gmail.com>
2022-06-03 00:02:16 +0300

    add model AnswersToQuestionView

---
+++ AutoSchool/Models/Views/AnswerView.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoSchool.Models.Views
+{
+    public class AnswerView
+    {
+        public long AnswerId { get; set; }
+        public string TextAnswer { get; set; }
+    }
+}
---
+++ AutoSchool/Models/Views/AnswersToQuestionView.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoSchool.Models.Views
+{
+    public class AnswersToQuestionView
+    {
+        public long QuestionId { get; set; }
+        public string QuestionText { get; set; }
+
+        public List<AnswerView> Answers { get; set; }
+    }
+}

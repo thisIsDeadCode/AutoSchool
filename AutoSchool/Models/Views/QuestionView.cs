e50441b368efadc670a10746486bab053bcfef99
Ilya Gubanov <ilya.gubanov.4.7.8@gmail.com>
2022-06-02 21:52:20 +0300

    fix question model

---
+++ AutoSchool/Models/Views/QuestionView.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoSchool.Models.Views
+{
+    public class QuestionView
+    {
+        public long Id { get; set; }
+        public long QuestionImageId { get; set; }
+        public string QuestionText { get; set; }
+    }
+}

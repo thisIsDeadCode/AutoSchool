57a309481d63e81200aefe2f66d490bce919501f
Ilya Gubanov <ilya.gubanov.4.7.8@gmail.com>
2022-06-05 22:51:21 +0300

    add GetLectureByDirection

--- AutoSchool/Controllers/LectureController.cs
+++ AutoSchool/Controllers/LectureController.cs
@@ -52,7 +52,7 @@ namespace AutoSchool.Controllers
 
         [HttpGet]
         [Route("Lecture/Get")]
-        public async Task<ActionResult<LectureResponse>> Get(long Id)
+        public async Task<ActionResult<LectureResponse>> Get(long Id, long? themeId = null)
         {
             Lecture? lecture = await _dbContext.Lectures.FirstOrDefaultAsync(x => x.Id == Id);
 
@@ -62,6 +62,15 @@ namespace AutoSchool.Controllers
             {
                 LectureResponse lectureView = lecture.ConvertLectureToLectureView();
 
+                //if(themeId != null)
+                //{
+                //    bool hasNext = false;
+                //    bool hasPrevious = false;
+                //    var lectures = _dbContext.Lectures.OrderBy(x => x.Id).Where(x => x.ThemeId == themeId).ToList();
+                //    var indexCurrentLecture = lectures.IndexOf(lecture);
+
+                //}
+
                 await _historyService.SaveTohistory(userDb, lecture);
 
                 return lectureView;
@@ -69,6 +78,74 @@ namespace AutoSchool.Controllers
             else
             {
                 return StatusCode(404);
+            }
+        }
+
+        [HttpGet]
+        [Route("Lecture/GetLectureByDirection")]
+        public async Task<ActionResult<LectureResponse>> GetLectureByDirection(long themeId, long currentLectureId, string direction)
+        {
+            User? userDb = await _dbContext.Users.FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+            var lectures = _dbContext.Lectures.OrderBy(x => x.Id).Where(x => x.ThemeId == themeId).ToList();
+            var lecture = lectures.FirstOrDefault(x => x.Id == currentLectureId);
+
+            if(userDb != null && lecture != null)
+            {
+                var indexCurrentLecture = lectures.IndexOf(lecture);
+
+                Lecture resultLecture;
+                bool hasNext = false;
+                bool hasPrevious = false;
+                var countLecture = lectures.Count();
+
+                if (direction == "next")
+                {
+                    if (countLecture - 1 == indexCurrentLecture)
+                    {
+                        return StatusCode(StatusCodes.Status404NotFound);
+                        
+                    }
+                    else
+                    {
+                        if(countLecture - 1 > indexCurrentLecture + 2 )
+                        {
+                            hasNext = true; 
+                        }
+                        resultLecture = lectures[indexCurrentLecture + 1];
+                    }
+                }
+                else if (direction == "previous")
+                {
+                    if (indexCurrentLecture == 0)
+                    {
+                        return StatusCode(StatusCodes.Status404NotFound);
+                    }
+                    else
+                    {
+                        if (countLecture >= 2)
+                        {
+                            hasPrevious = true;
+                        }
+                        resultLecture = lectures[indexCurrentLecture - 1];
+                    }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+                LectureResponse resultLectureView = resultLecture.ConvertLectureToLectureView();
+                resultLectureView.HasPrevious = hasPrevious;
+                resultLectureView.NasNext = hasNext;
+
+                await _historyService.SaveTohistory(userDb, resultLecture);
+
+                return resultLectureView;
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
             }
         }
 
--- AutoSchool/Models/Views/LectureResponse.cs
+++ AutoSchool/Models/Views/LectureResponse.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public string TextHTML { get; set; }
         public string? Description { get; set; }
+        public bool HasPrevious { get; set; }
+        public bool NasNext { get; set; }
     }
 }
